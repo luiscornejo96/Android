5077bd6ba7a4c0ee62e526ba971b4bf009769fbc
Luis Cornejo <luis.cornejo.96@hotmail.com>
2019-08-20 07:47:31 -0500

    Se modifica MainActivity, prueba del parseo a tiempo unix y diferencia en días

--- Prueba/app/src/main/java/luis/cornejo/prueba/MainActivity.java
+++ Prueba/app/src/main/java/luis/cornejo/prueba/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v4.app.NotificationCompat;
@@ -24,56 +26,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         noti = findViewById(R.id.Btn_Notificacion);
         noti.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                NotificationManager notificationManager = (NotificationManager) getSystemService(getApplicationContext().NOTIFICATION_SERVICE);
-                Log.i("SDK:", " " + Build.VERSION.SDK_INT);
-                NotificationCompat.Builder mBuilder =  new NotificationCompat.Builder(MainActivity.this, null);
-                // Create an explicit intent for an Activity in your app
-                Intent intent = new Intent(MainActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, 0);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-                {
-                    String channelId = "Channel_Id";
-                    CharSequence name = "Ofertas";
-                    String description = "Hola mundo on Android";
-                    int importance = NotificationManager.IMPORTANCE_HIGH;
 
-                    NotificationChannel notificationChannel =  new NotificationChannel(channelId, name, importance);
-                    notificationChannel.setDescription(description);
-                    notificationChannel.enableLights(true);
-                    notificationChannel.setLightColor(Color.GREEN);
-                    notificationChannel.enableVibration(true);
-                    notificationChannel.setVibrationPattern(new long[] {100, 200, 300, 400, 500, 400, 300, 200, 400});
-
-                    notificationManager.createNotificationChannel(notificationChannel);
-
-                    mBuilder = new NotificationCompat.Builder(MainActivity.this, channelId);
-                    mBuilder.setSmallIcon(R.mipmap.ic_launcher)
-                            .setContentIntent(pendingIntent)
-                            .setContentTitle("Modificación de Embarque")
-                            .setContentText("La modificación de X, fue exitosa.")
-                            .setAutoCancel(true);
-
-                    notificationManager.notify( cont++, mBuilder.build());
-
-
-                }
-                else
-                {
-                    mBuilder.setSmallIcon(R.mipmap.ic_launcher)
-                            .setContentIntent(pendingIntent)
-                            .setContentTitle("Modificación de Embarque")
-                            .setContentText("La modificación de X, fue exitosa.")
-                            .setAutoCancel(true);
-
-                    NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(MainActivity.this);
-                    notificationManagerCompat.notify(cont++, mBuilder.build());
-                }
             }
         });
     }
